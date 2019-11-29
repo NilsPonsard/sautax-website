d4b722de8db940ba6c73daec72601a117bbfdba2
Nils Ponsard <nils.ponsard@etu.univ-amu.fr>
2019-11-29 15:53:01 +0100

    contact

--- js/main.js
+++ js/main.js
@@ -70,5 +70,5 @@ function loaded() {
     dropdown = document.getElementById("nav-dropdown")
     console.log(document.body.style.transitionDuration)
     document.body.style.transitionDuration = "1s"
-    document.getElementById()
+    document.getElementById("img-acceuil").style.transitionDuration = "1s"
 }
